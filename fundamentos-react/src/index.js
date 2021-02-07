389b86b59556d17c00776e834dd3bb0a312256b6
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-07 17:19:31 -0300

    advancing em react

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,14 @@ import ReactDOM from  'react-dom'
 import React from 'react'
 import './index.css'
 import Primeiro from './components/basics/Primeiro'
-
+import ComParametro from './components/basics/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
+        <ComParametro titulo="Segundo Componente"
+        aluno="vinicius Aquino"
+        nota='5.3'/>
     </div>,
     document.getElementById('root')
 )
