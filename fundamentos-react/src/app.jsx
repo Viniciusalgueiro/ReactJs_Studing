2ffa9ebf82cdb50b9c7d88517c4083271b7ccc3f
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-09 23:45:51 -0300

    finishing class 17

--- fundamentos-react/src/app.jsx
+++ fundamentos-react/src/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Aleatorio from './components/basics/Aleatorio'
 import Primeiro from './components/basics/Primeiro'
 import ComParametro from './components/basics/ComParametro'
 import Fraguimentos from './components/basics/Fraguimentos'
@@ -9,6 +10,7 @@ import Fraguimentos from './components/basics/Fraguimentos'
 export default () => (
     <div id='app'>
         <h1>Fundamentos React </h1>
+        <Aleatorio min={1} max={60} />
         <Fraguimentos />
         <ComParametro
             titulo="Segundo Componente"
