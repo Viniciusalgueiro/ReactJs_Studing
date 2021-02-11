724000bbf6092070d5d61e4e41075e11dcc7570d
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-11 00:30:33 -0300

    finishing class 24

--- fundamentos-react/src/app.jsx
+++ fundamentos-react/src/app.jsx
@@ -5,21 +5,34 @@ import Primeiro from './components/basics/Primeiro'
 import ComParametro from './components/basics/ComParametro'
 import Fraguimentos from './components/basics/Fraguimentos'
 import Card from './components/layout/card'
+import Familia from './components/basics/familia'
+import ListaAlunos from './components/Repeticao/ListaAlunos'
+import FamiliaMembro from './components/basics/FamiliaMembro'
 import './App.css'
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
     <div className='App'>
-        <h1>Fundamentos React </h1>
+        <h1>Fundamentos React </h1>        
         <div className="Cards">
-            <Card titulo="#04 - Desafio Aleatorio"  color='#FA6900'>
-                <Aleatorio min={1} max={60} />
+        <Card titulo="#06 - Lista Alunos" color='#53BBF4 '>
+                <ListaAlunos/>
+            </Card>
+            <Card titulo="#05 - Componente com Filhos" color='#F0A830'>
+                <Familia sobrenome='Silva'>
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
+                </Familia>
+            </Card>
+            <Card titulo="#04 - Desafio Aleatorio" color='#FA6900'>
+                <Aleatorio min={1} max={120} />
             </Card>
             <Card titulo="#03 - Fragmentos" color='#E94C6F'>
                 <Fraguimentos />
             </Card>
-            <Card titulo="#02 - ComParametro"  color='#E8B71A'> 
+            <Card titulo="#02 - ComParametro" color='#E8B71A'>
                 <ComParametro
                     titulo="Segundo Componente"
                     aluno="vinicius Aquino"
